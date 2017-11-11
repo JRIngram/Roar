5167f7e72b275388bd23a2bb43b2657291d717dd
JRIngram <jr-ingram@hotmail.co.uk>
2017-11-11 19:44:35 +0000

    SQL script to create underlying DB

---
+++ createDB.sql
@@ -0,0 +1,63 @@
+DROP DATABASE roar;
+
+CREATE DATABASE roar;
+
+use roar;
+
+
+CREATE TABLE user(
+
+	user_id MEDIUMINT PRIMARY KEY AUTO_INCREMENT,
+
+	username VARCHAR(255),
+
+	password VARCHAR(256),
+
+	email VARCHAR(256) UNIQUE,
+
+	dateOfBirth DATE,
+
+	user_role ENUM('USER', 'ADMIN', 'MOD')
+);
+
+
+
+CREATE TABLE post(
+
+	post_id MEDIUMINT PRIMARY KEY AUTO_INCREMENT,
+
+	postedBy MEDIUMINT,
+
+	likes MEDIUMINT,
+
+	content VARCHAR(140),
+
+	FOREIGN KEY (postedBy) REFERENCES user(user_id)
+);
+
+
+
+CREATE TABLE postLike(
+
+	post_id MEDIUMINT,
+
+	user_id MEDIUMINT,
+
+	FOREIGN KEY (post_id) REFERENCES post(post_id),
+
+	FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+
+
+CREATE TABLE commentLike(
+
+	comment_id MEDIUMINT,
+
+	user_id MEDIUMINT,
+
+	FOREIGN KEY(post_id) REFERENCES comment(comment_id),
+
+	FOREIGN KEY(user_id) REFERENCES user(user_id)
+
+);
